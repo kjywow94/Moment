b686a10468d45b58a6117f31892e3490e70974da
bora <capsulejay963@naver.com>
2019-10-16 17:56:42 +0900

    user api create

--- back_end/TravelMaker/src/main/java/com/travelmaker/TravelMakerApplication.java
+++ back_end/TravelMaker/src/main/java/com/travelmaker/TravelMakerApplication.java
@@ -1,9 +1,12 @@
 package com.travelmaker;
 
+import org.mybatis.spring.boot.autoconfigure.MybatisAutoConfiguration;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+//@EnableAutoConfiguration(exclude = {MybatisAutoConfiguration.class})
 public class TravelMakerApplication {
 
 	public static void main(String[] args) {
