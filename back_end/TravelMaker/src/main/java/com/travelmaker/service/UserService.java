15b60750b8b114a79a8ee864b7870e90762ff258
bora <capsulejay963@naver.com>
2019-10-18 13:45:30 +0900

    S1P3151008-48

--- back_end/TravelMaker/src/main/java/com/travelmaker/service/UserService.java
+++ back_end/TravelMaker/src/main/java/com/travelmaker/service/UserService.java
@@ -4,20 +4,28 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.travelmaker.dao.UserMapper;
 import com.travelmaker.dto.User;
 
 @Service
+@Transactional
 public class UserService {
 	
 	@Autowired
 	private UserMapper userMapper;
 	
-	//회원전체조회
+	//회원 전체 조회
 	public List<User> selectAllUser(){
 		return userMapper.selectAllUser();
 	}
+	
+	//정상 회원 전체 조회
+	public List<User> selectNotRemovedAllUser(){
+		return userMapper.selectNotRemovedAllUser();
+	}
+	
 	//로그인&회원조회
 	public User selectOneUser(User user) {
 		return userMapper.selectOneUser(user);
@@ -28,12 +36,12 @@ public class UserService {
 		return userMapper.insertUser(user);
 	}
 	
-	//회원수정
+	//회원수정/회원삭제
 	public int updateUser(User user) {
 		return userMapper.updateUser(user);
 	}
 	
-	//회원탈퇴
+	//회원탈퇴(보류)
 	public int deleteUser(int id) {
 		return userMapper.deleteUser(id);
 	}
