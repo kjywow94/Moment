e7607078e0bd3cd55f6d084980fec8a8478c9af7
bora <capsulejay963@naver.com>
2019-10-28 14:27:11 +0900

    feat 유저 프로필변경 유저 프로필 필요없는 기능 삭제 S1P3151008-78

--- front_end/src/views/components/profile/UsersProfile.vue
+++ front_end/src/views/components/profile/UsersProfile.vue
@@ -11,8 +11,7 @@
           />
         </div>
         <div class="name">
-          <h3 class="title">{{userProfile.userName}}</h3>
-          <h6>{{userProfile.gender}}, {{userProfile.age}}살</h6>
+          <h3 class="title">{{userProfile.nickname}}</h3>
           <md-button
             href="javascript:void(0)"
             class="md-just-icon md-simple md-twitter"
@@ -29,12 +28,11 @@
   </div>
   <div class="description text-center">
     <p>{{userProfile.about}}</p>
-    {{userProfile}}
   </div>
   <div class="profile-tabs">
     <tabs
-      :tab-name="['사진', '여행스타일', '여행후기']"
-      :tab-icon="['camera', 'favorite', 'message']"
+      :tab-name="['my writing']"
+      :tab-icon="['message']"
       plain
       nav-pills-icons
       color-button="info"
@@ -42,57 +40,14 @@
       <template slot="tab-pane-1">
         <div class="md-layout">
 
-          <!-- test -->
+          <!-- test 
            <div v-for="item in userImg">
             <img :src="item.imgData">
-           </div>
+           </div>-->
 
         </div>
       </template>
 
-      <template slot="tab-pane-2">
-        <div align="center">
-          <div class = "col-10 col-md-6 col-xl-6 font-size">
-            <div v-for="n in tasteIdx">
-              <div class="alert alert-info" style="background-color: rgba(255, 174, 236, 0.72); color:#076e7f;" >
-                <div class="container">
-                  <div style="font-weight: bold;">{{tasteTitle[n-1]}}</div>
-                  <div>{{tasteName[n-1][tasteSelect[n-1]]}}</div>
-                </div>
-              </div>
-            </div> 
-          </div>
-        </div>  
-      </template>
-
-      <template slot="tab-pane-3">
-        <div align="center">
-          <div class = "col-10 col-md-6 col-xl-6">
-            <div v-for="item in userReview">
-              <div class="alert alert-info" style="background-color: #3ddcf080; color:black;" >
-                <div class="container" align="left">
-                  <div>
-                    <b>날짜 : {{item.endDate}}</b>
-                    <div class="star-size"> <b>평점 :</b>
-                      <span style="color:yellow">
-                        <template v-for="n in item.grade">
-                          ★ 
-                        </template>
-                        <template v-for="n in (5-item.grade)">
-                          ☆ 
-                        </template>
-                      </span>
-                      <b>({{item.grade}}/5)</b>
-                    </div>
-                  </div>
-                  <hr class="hrColor">
-                  <div>{{item.review}}</div>
-                </div>
-              </div>
-            </div> 
-          </div>
-        </div>  
-      </template>
     </tabs>
   </div>
 </div>
@@ -113,20 +68,21 @@ export default {
       email : 'coin@ggg',
       userImg: [],
       userProfile: [],
-      tasteIdx : 7,
-      tasteTitle: ['구경', '사진', '음식', '사람', '계획', '쇼핑' ,'경비'],
-      tasteName:[['번화가 좋아요','자연이 좋아요'],['남는건 사진뿐','눈으로 찍을래']
-                ,['맛집 좋아요','아무거나 잘먹어요'],['다양한 사람과 많이 만나기','소수의 사람과 친밀하게']
-                ,['계획적인 여행','즉흥적인 여행'],['쇼핑 좋아요','쇼핑 싫어요']
-                ,['경비는 여유롭게 사용','경비는 알뜰살뜰하게 사용']],
-      tasteSelect: [],
-      userReview: [],
-      tabPane1: [
-        { image: require("@/assets/img/examples/studio-1.jpg") },
-        { image: require("@/assets/img/examples/studio-2.jpg") },
-        { image: require("@/assets/img/examples/studio-4.jpg") },
-        { image: require("@/assets/img/examples/studio-5.jpg") }
-      ]
+      // tasteIdx : 7,
+      // tasteTitle: ['구경', '사진', '음식', '사람', '계획', '쇼핑' ,'경비'],
+      // tasteName:[['번화가 좋아요','자연이 좋아요'],['남는건 사진뿐','눈으로 찍을래']
+      //           ,['맛집 좋아요','아무거나 잘먹어요'],['다양한 사람과 많이 만나기','소수의 사람과 친밀하게']
+      //           ,['계획적인 여행','즉흥적인 여행'],['쇼핑 좋아요','쇼핑 싫어요']
+      //           ,['경비는 여유롭게 사용','경비는 알뜰살뜰하게 사용']],
+      // tasteSelect: [],
+      userReview: []
+      // ,
+      // tabPane1: [
+      //   { image: require("@/assets/img/examples/studio-1.jpg") },
+      //   { image: require("@/assets/img/examples/studio-2.jpg") },
+      //   { image: require("@/assets/img/examples/studio-4.jpg") },
+      //   { image: require("@/assets/img/examples/studio-5.jpg") }
+      // ]
     };
   },
   props: {
@@ -136,33 +92,34 @@ export default {
     }
   },
   methods: {
-    getCalculateAge : function(birthday) {
-      let birth = new Date(birthday);
-      let today = new Date();
-      let years = today.getUTCFullYear() - birth.getUTCFullYear();
+    // getCalculateAge : function(birthday) {
+    //   let birth = new Date(birthday);
+    //   let today = new Date();
+    //   let years = today.getUTCFullYear() - birth.getUTCFullYear();
 
-      birth.setFullYear(today.getFullYear());
-      if(today < birth) years--; 
-      return years;
-    },
-    getTasteContent: function(index) {
-      return "taste" + index + "";
-    },
+    //   birth.setFullYear(today.getFullYear());
+    //   if(today < birth) years--; 
+    //   return years;
+    // },
+    // getTasteContent: function(index) {
+    //   return "taste" + index + "";
+    // },
     getDate: function(dates) {
       let date = new Date(dates);
       return date.getUTCFullYear() + "." + (date.getMonth()) + "." + date.getDate();
-    },
-    uploadImg(){
-      console.log("refs : ", this.$refs);
-      this.$refs.inputRef.click();
     }
+    // ,
+    // uploadImg(){
+    //   console.log("refs : ", this.$refs);
+    //   this.$refs.inputRef.click();
+    // }
   },
   mounted() {
     //사용자의 정보를 가져온다.
     UserProfileService.getUserProfile(this.uid)
       .then(userProfile => {
-        userProfile.data.gender = (userProfile.data.gender == 'M' ? '남자' : '여자');
-        userProfile.data.age = this.getCalculateAge(userProfile.data.birthday);
+        // userProfile.data.gender = (userProfile.data.gender == 'M' ? '남자' : '여자');
+        // userProfile.data.age = this.getCalculateAge(userProfile.data.birthday);
         this.userProfile = userProfile.data;
 
         //선택한 여행스타일 번호를 배열에 담아준다.
@@ -176,26 +133,26 @@ export default {
       });
 
     //사용자 후기를 가져온다.
-    UserProfileService.getUserReview(this.uid)
-      .then(userReview => {
-        this.userReview = userReview.data;
-        for(let i = 0; i < this.userReview.length; i++){
-          this.userReview[i].endDate = this.getDate(this.userReview[i].endDate);
-        }
-      })
-      .catch(err => {
-        console.log("userReview error : ", err);
-      });
+    // UserProfileService.getUserReview(this.uid)
+    //   .then(userReview => {
+    //     this.userReview = userReview.data;
+    //     for(let i = 0; i < this.userReview.length; i++){
+    //       this.userReview[i].endDate = this.getDate(this.userReview[i].endDate);
+    //     }
+    //   })
+    //   .catch(err => {
+    //     console.log("userReview error : ", err);
+    //   });
 
     //사용자의 사진을 가져온다.
-    UserProfileService.getUserImage(this.email)
-      .then(userImg => {
-        this.userImg = userImg.data;
-        console.log("userImg : ",userImg);
-    })
-    .catch(err => {
-      console.log("UserProfileService error : ", err);
-    });
+    // UserProfileService.getUserImage(this.email)
+    //   .then(userImg => {
+    //     this.userImg = userImg.data;
+    //     console.log("userImg : ",userImg);
+    // })
+    // .catch(err => {
+    //   console.log("UserProfileService error : ", err);
+    // });
   }
 };
 </script>
