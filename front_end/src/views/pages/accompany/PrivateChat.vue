dc7bf5ac8e62e8a204f9916fd89f4306b5c920c1
김선일 <rain6781@naver.com>
2019-10-30 16:18:02 +0900

    feat 개인대 개인 채팅 알림 구현 알림을 위한 chat_list 테이블 생성 및 메세지 수신시 알림 출력 S1P3151008-79

--- front_end/src/views/pages/accompany/PrivateChat.vue
+++ front_end/src/views/pages/accompany/PrivateChat.vue
@@ -1,6 +1,21 @@
 <template>
   <div class="wrapper">
     <parallax class="section page-header header-filter" :style="headerStyle">
+      <div class="main main-raised " style="overflowY:scroll;  height:200px;">
+      <div v-for="tmp in chatList">
+      <router-link :to="/privateChat/ + tmp['uid']">
+        <md-button class="md-rose md-just-icon md-round" aria-disabled="true">{{tmp['notification']}}</md-button>
+        <md-button> {{tmp['nickname']}} </md-button>
+        </router-link>
+      </div></div>
+      <div class="main main-raised " style="overflowY:scroll;  height:200px;">
+
+      <div  v-for="tmp in userList">
+        <router-link :to="/privateChat/ + tmp['id']">
+        <md-button> {{tmp['nickname']}} </md-button>
+        </router-link>
+      </div>
+      </div>
     </parallax>
     <div class="main main-raised">
       <div class="section section-contacts">
@@ -28,9 +43,13 @@
 
 <script>
 import ChatService from "@/services/ChatService.js";
+import ChatListService from "@/services/ChatListService.js";
+import UserService from "@/services/UserService.js";
 export default {
   components: {
-    ChatService
+    UserService,
+    ChatService,
+    ChatListService
   },
   bodyClass: "landing-page",
   props: {
@@ -56,8 +75,10 @@ export default {
         stompClient: null,
         msg:"",
         chat:[],
-        id: this.$route.params.id,
-        uid: this.$store.state.user.id
+        id: parseInt(this.$route.params.id),
+        uid: this.$store.state.user.id,
+        chatList: [],
+        userList: []
     };
   },
   computed: {
@@ -74,10 +95,22 @@ export default {
 
   methods : {
     async init(){
+      this.getChatList();
+      setInterval(() => {
+        ChatListService.checkNotification(this.id, this.uid);
+        this.getChatList();
+      }, 1000);
+      ChatListService.checkNotification(this.id, this.uid);
+      let users = await UserService.getAllUsers();
+      this.userList = users['data'];
+      for(let i = 0 ; i < this.userList.length ; i++){
+        this.userList[i]['nickname'] = await this.getNickname(this.userList[i]['id'])
+      }
+      
+      
+
       this.chat = await ChatService.getPrivateChats(this.id, this.uid);
       this.chat = this.chat['data'];
-      console.log(this.uid)
-      console.log(this.id)
       var socket = new SockJS('http://192.168.100.49:9090/websocket');
       this.stompClient = Stomp.over(socket);
       this.stompClient.debug = function(str) {};
@@ -86,6 +119,7 @@ export default {
       await this.stompClient.connect({}, function (frame) {
         scope.stompClient.subscribe('/topic/privateChat/' + scope.uid, function (chat) {
           let msg = JSON.parse(chat.body);
+          // ChatListService.checkNotification(scope.id, scope.uid);
           scope.chat.push(msg);
         });
       });
@@ -98,15 +132,24 @@ export default {
       }
         let chat = {
             'uid' : this.uid,'timestamp':new Date(), 'context': this.$store.state.user.nickname + " : " + this.msg,
-            'isRemoved' : 'N', 'target' : this.id, type : "MSG"}
+            'isRemoved' : 'N', 'target' : this.id, type : "MSG"};
         this.stompClient.send("/app/privateChat", {}, JSON.stringify(chat));
         this.chat.push(chat);
         ChatService.insertChat(chat);
+        ChatListService.insertOrUpdateNotification(this.uid, this.id,this.$store.state.user.nickname);
         this.msg = "";
         this.setChatDivBottom();
     },
     setChatDivBottom(){
       document.getElementById("chat").scrollTop = document.getElementById("chat").scrollHeight; 
+    },
+    async getNickname(uid){
+      let user = await UserService.getUserById(uid);
+      return user['data']['nickname'];
+    },
+    async getChatList(){
+      let tmp = await ChatListService.getNotificationList(this.uid);
+      this.chatList = tmp['data'];
     }
   }
 };
