39cdecf24cf90505944a645a41a088425e95334f
bora <capsulejay963@naver.com>
2019-11-05 17:21:24 +0900

    feat 배포 배포 수정 S1P3151008-92

--- front_end/public/constants.js
+++ front_end/public/constants.js
@@ -1,3 +1,3 @@
-//const API_BASE_URL = "http://13.209.9.195:8888";
-const API_BASE_URL = "http://localhost:9090";
+const API_BASE_URL = "http://13.209.9.195:8888";
+//const API_BASE_URL = "http://localhost:8888";
 //백엔드 IP 주소
